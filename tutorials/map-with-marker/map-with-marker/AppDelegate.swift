37aad65a69c430f6aa310e2aabd3f901958325da
christopher daniel <christopherdaniel13397@gmail.com>
2017-09-27 21:35:30 +0530

    Update AppDelegate.swift

--- tutorials/map-with-marker/map-with-marker/AppDelegate.swift
+++ tutorials/map-with-marker/map-with-marker/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDPcaDLmtj4BEFE5rYtBtVNAJMe5WdnXnM")
+    GMSServices.provideAPIKey("")
     return true
   }
 
